5f5efffb3f167abb814031caeb70168aa561655f
JudSL <sl.jdth@gmail.com>
2019-06-14 02:44:27 -0500

    Agregando secciones Cifrar y Descrifrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+//Funciones para aparecer y desaparecer pantallas con id
+  const hideSection = (id) => {
+    document.getElementById(id).classList.add('hide');
+  }
+  const showSection = (id) => {
+    document.getElementById(id).classList.remove('hide');
+  }
 //First page
 const startButton = document.getElementById('loginButton');
 let policeNumber = document.getElementById('registrationPersonal');
@@ -5,20 +12,23 @@ let policeNumber = document.getElementById('registrationPersonal');
 const getRegistration = () => {
   const registration = document.getElementById('registrationBox').value;
   policeNumber.innerHTML = registration;
-  document.getElementById('login-page').classList.add('hide');
-  document.getElementById('options-page').classList.remove('hide');
+  // document.getElementById('login-page').classList.add('hide');
+  // document.getElementById('options-page').classList.remove('hide');
+  hideSection('login-page');
+  showSection('options-page');
 }
-
 loginButton.addEventListener('click',getRegistration);
-
+//Abrir la sección CIFRAR
 const cipher = document.getElementById('cipherButton')
+const openCipherPage = () => {
+  hideSection('options-page');
+  showSection('cipher-page');
+}
+cipher.addEventListener('click', openCipherPage)
+//Abrir la sección DESCIFRAR
 const decipher = document.getElementById('decipherButton')
-
-
-const hideSection = () => {
-  document.getElementById(id).classList.add('hide');
+const openDecipherPage = () => {
+  hideSection('options-page');
+  showSection('decipher-page');
 }
-
-const showSection = (id) => {
-  document.getElementById(id).classList.remove('hide');
-}
+decipher.addEventListener('click', openDecipherPage)
